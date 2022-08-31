28c478f00abd3d74200eaf85033d02a752ee037c
faizan altaf <faizan@Faizans-MacBook-Pro-2.local>
2022-08-31 19:10:05 +0500

    Add template3

--- resume_demo/settings/base.py
+++ resume_demo/settings/base.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
-    
+
     "storages",
 ]
 
@@ -128,13 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media')
 ]
- 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
