9263729a04f839bfaf9cff6db439e68c62ad0c9f
faizan altaf <faizan@Faizans-MacBook-Pro-2.local>
2022-08-31 16:20:45 +0500

    Add AWS settings

--- resume_demo/settings/base.py
+++ resume_demo/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from distutils.command.config import config
 from pathlib import Path
 import os
+from decouple import config
 
 import dj_database_url
 from dotenv import load_dotenv, find_dotenv
