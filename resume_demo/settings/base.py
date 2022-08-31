811bcb4c89dd61cce3cd4408661fd8fa2bd9b556
faizan altaf <faizan@Faizans-MacBook-Pro-2.local>
2022-08-31 11:54:09 +0500

    Settings

--- resume_demo/settings/base.py
+++ resume_demo/settings/base.py
@@ -15,8 +15,6 @@ import os
 import dj_database_url
 from dotenv import load_dotenv, find_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
 
 
 from pathlib import Path
